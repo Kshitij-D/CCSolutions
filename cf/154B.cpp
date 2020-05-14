b17fe35c7321bd7c7b1eb3f90d75bf0d3abf4d67
Kshitj-D <39673907+Kshitj-D@users.noreply.github.com>
2020-05-14 22:56:59 +0530

    TLE on case 24

--- cf/154B.cpp
+++ cf/154B.cpp
@@ -1,18 +1,91 @@
+//Getting TLE
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
-int gcd(int a, int b){
-    if(b == 0){return a;}
-    return gcd(b, a%b)
-}
+int fac[100001];
+int on[100001];
+
 
 int main(){
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+    vector<int> fac(100001,0);
+    vector<int> on(100001,0);
     int m, n;
     cin >> n >> m;
+
+    vector<bool> p(n,true);
+    p[0] = p[1] = false;
+
+    for (int i = 2; i * i <= n; i++) {
+    if (p[i]) {
+        for (int j = i * i; j <= n; j += i)
+            p[j] = false;
+    }
+}
+
     for (int i = 0; i < m; i++){
+        int t;
+        vector<int> f;
+        char c;
+        cin >> c >> t;
         
+        if(c == '-'){
+            if(on[t] == 1){
+                if(!p[t]){
+                    for(int j = 2; j <= t; j++){
+                        if(p[j]){
+                            if(t % j == 0){
+                                fac[j] = 0;
+                            }
+                        }
+                    }
+                }
+                else{fac[t] = 0;}
+                on[t] = 0;
+                cout << "Success" << '\n';
+            }
+            else{cout << "Already off" << '\n';}
+        }
+
+        if(c == '+'){
+            if(on[t]){
+                cout << "Already on" << '\n';
+            }
+            else{
+                if(!p[t]){
+                    for(int c = 2; c <= t; c++){
+                        if(p[c]){
+                            if(t % c == 0){
+                                f.push_back(c);                      
+                            }
+                        }
+                    }
+                }
+                else{f.push_back(t);}
+
+                bool v = true;
+                for(int x : f){
+                    if(fac[x] > 0){
+                        cout << "Conflict with "<< fac[x] <<'\n';
+                        v = false;
+                    break;
+                    }
+                }
+
+                if(v){
+                    for(int x : f){
+                    fac[x] = t;
+                    }
+                    on[t] = 1;
+                    cout << "Success" << '\n';
+                }
+            }
+        }
     }
     
-    return 0;
+    return 0; 
 }
